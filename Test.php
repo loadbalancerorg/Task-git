782181a316fcf6579aa88fb6298ae124f33f7608
Kalyani <100567719+KPUL@users.noreply.github.com>
2022-03-11 12:34:52 +0000

    Test Class

---
+++ Test.php
@@ -0,0 +1,12 @@
+<?php
+
+class Test
+{
+    public static function test()
+    {
+        echo "Hello World";
+        echo "Welcome";
+        echo "Hello World Again";
+        echo "Welcome Again";
+    }
+}
